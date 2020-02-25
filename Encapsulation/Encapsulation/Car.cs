8aa4349ab226b333fc1a204f0c40cb8aaa357342
Lytherus <60918738+Lytherus@users.noreply.github.com>
2020-02-25 15:48:15 +0000

    Add files via upload

---
+++ Encapsulation/Encapsulation/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation
+{
+    class Car
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public double EngineSize { get; set; }
+    }
+}
---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.EngineSize = 6.2;
+            Console.WriteLine(car.EngineSize);
+            car.Name = "Subaru";
+            Console.WriteLine(car.Name);
+            car.Year = 2003;
+            Console.WriteLine(car.Year);
+        }
+    }
+}
